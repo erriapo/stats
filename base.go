766292dbe6aaacf5610163eb9b8ef7f1f91a710c
K46 guy <mycodingstuff@yahoo.com>
2017-07-14 14:23:10 -0700

    gitignoring files

--- base.go
+++ base.go
@@ -5,7 +5,18 @@ type Sink interface {
 	Name() string
 }
 
+type Variability interface {
+	Min() float64
+	Max() float64
+	Mean() float64
+	StandardDeviation() float64
+}
+
+const welford = "B. P. Welford's 1962 algorithm"
+
 type WelfordSink struct {
+	Sink
+	Variability
 }
 
 func NewSink() *WelfordSink {
@@ -14,4 +25,8 @@ func NewSink() *WelfordSink {
 
 func (s *WelfordSink) Push(x float64) error {
 	return nil
+}
+
+func (s WelfordSink) Name() string {
+	return welford
 }
