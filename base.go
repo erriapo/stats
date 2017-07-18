01e2a097070336ee0544064680d71487bc239c20
K46 guy <mycodingstuff@yahoo.com>
2017-07-18 09:08:08 -0700

    added unit tests

--- base.go
+++ base.go
@@ -1,5 +1,10 @@
 package stats
 
+import (
+	"errors"
+	"math"
+)
+
 type Sink interface {
 	Push(x float64) error
 	Name() string
@@ -10,13 +15,25 @@ type Variability interface {
 	Max() float64
 	Mean() float64
 	StandardDeviation() float64
+	Count() int
 }
 
 const welford = "B. P. Welford's 1962 algorithm"
 
+// ErrInvalidValue means that the item you supplied is a NaN or an Inf
+// which is considered illegal.
+var ErrInvalidValue = errors.New("NaN or +-Inf are not allowed")
+
+// @TODO make this goroutine-safe
 type WelfordSink struct {
 	Sink
 	Variability
+	observationMin float64
+	count          int
+	mOld           float64
+	mNew           float64
+	nNew           float64
+	nOld           float64
 }
 
 func NewSink() *WelfordSink {
@@ -24,9 +41,43 @@ func NewSink() *WelfordSink {
 }
 
 func (s *WelfordSink) Push(x float64) error {
+	if math.IsNaN(x) || math.IsInf(x, 0) {
+		return ErrInvalidValue
+	}
+
+	s.count++
+
+	if s.count == 1 {
+		s.mOld = x
+		s.mNew = x
+		s.nOld = 0.0
+	} else {
+		s.mNew = s.mOld + (x-s.mOld)/float64(s.count)
+		s.nNew = s.nOld + (x-s.mOld)*(x-s.mNew)
+
+		s.mOld = s.mNew
+		s.nOld = s.mNew
+	}
+
 	return nil
 }
 
-func (s WelfordSink) Name() string {
+func (s *WelfordSink) Name() string {
 	return welford
+}
+
+func (s *WelfordSink) Min() float64 {
+	return s.observationMin
+}
+
+func (s *WelfordSink) Count() int {
+	return s.count
+}
+
+func (s *WelfordSink) Mean() float64 {
+	if s.count > 0 {
+		return s.mNew
+	} else {
+		return float64(s.count)
+	}
 }
---
+++ base_test.go
@@ -0,0 +1,82 @@
+package stats
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"reflect"
+	"testing"
+)
+
+// a very small number
+var EPSILON float64
+
+func init() {
+	EPSILON = math.Nextafter(1, 2) - 1
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("testmain")
+	fmt.Println()
+	os.Exit(m.Run())
+}
+
+func TestTypes(t *testing.T) {
+	var s Sink
+	s = NewSink()
+	fmt.Println(reflect.TypeOf(s))
+
+	_, ok := s.(Sink)
+	fmt.Printf("%v#\n", ok)
+	if !ok {
+		t.Error("not Sink")
+	}
+	_, ok = s.(Variability)
+	if !ok {
+		t.Error("not Variability")
+	}
+
+	got := s.Name()
+	if got != welford {
+		t.Errorf("got '%v' ; want '%v'", got, welford)
+	}
+}
+
+func isCloseTo(x, target float64) bool {
+	fmt.Printf("%g // %g\n\n", x-target, EPSILON)
+	return math.Abs(x-target) < EPSILON
+}
+
+func TestZeroValue(t *testing.T) {
+	var v Variability
+	v = NewSink()
+	fmt.Println(reflect.TypeOf(v))
+
+	_, ok := v.(Variability)
+	if !ok {
+		t.Error("not Variability")
+	}
+	min := v.Min()
+	if !isCloseTo(min, 0.000000) {
+		t.Errorf("got '%v' ; want '%v'", min, 0.00)
+	}
+}
+
+func TestValidValuesAndCounts(t *testing.T) {
+	s := NewSink()
+	if err := s.Push(math.NaN()); err == nil {
+		t.Errorf("expected %v ; got nil", err)
+	}
+}
+
+func TestMean(t *testing.T) {
+	s := NewSink()
+	s.Push(1.0)
+	s.Push(3.0)
+	s.Push(5.0)
+	mean := s.Mean()
+	fmt.Printf("mean is %g\n", mean)
+	if !isCloseTo(mean, 3.0) {
+		t.Errorf("got '%v' ; want '%v'", mean, 3.0)
+	}
+}
