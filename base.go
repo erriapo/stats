514d1341b40031df264f3ff89b37d7e112922842
K46 guy <mycodingstuff@yahoo.com>
2017-07-21 09:08:40 -0700

    working standard deviation

--- base.go
+++ base.go
@@ -5,11 +5,13 @@ import (
 	"math"
 )
 
+// Sink accepts a stream of observations.
 type Sink interface {
 	Push(x float64) error
 	Name() string
 }
 
+// Variability is a measure of how spread out is your dataset.
 type Variability interface {
 	Min() float64
 	Max() float64
@@ -24,11 +26,13 @@ const welford = "B. P. Welford's 1962 algorithm"
 // which is considered illegal.
 var ErrInvalidValue = errors.New("NaN or +-Inf are not allowed")
 
+// WelfordSink is an implementation of a Sink and Variability
 // @TODO make this goroutine-safe
 type WelfordSink struct {
 	Sink
 	Variability
 	observationMin float64
+	observationMax float64
 	count          int
 	mOld           float64
 	mNew           float64
@@ -36,10 +40,27 @@ type WelfordSink struct {
 	nOld           float64
 }
 
+// NewSink returns a pointer to an implementation of interfaces Sink & Variability
 func NewSink() *WelfordSink {
-	return &WelfordSink{observationMin: math.Inf(-1)}
+	return &WelfordSink{
+		observationMin: math.Inf(-1),
+		observationMax: math.Inf(1)}
 }
 
+func (s *WelfordSink) variance() float64 {
+	if s.count > 1 {
+		return s.nNew / float64(s.count-1)
+	}
+	return 0.0
+}
+
+// StandardDeviation returns the running sd of the observations
+func (s *WelfordSink) StandardDeviation() float64 {
+	return math.Sqrt(s.variance())
+}
+
+// Push accepts an observation.
+// On failure, it returns an ErrInvalidValue.
 func (s *WelfordSink) Push(x float64) error {
 	if math.IsNaN(x) || math.IsInf(x, 0) {
 		return ErrInvalidValue
@@ -51,12 +72,13 @@ func (s *WelfordSink) Push(x float64) error {
 		s.mOld = x
 		s.mNew = x
 		s.nOld = 0.0
+		s.nNew = 0.0
 	} else {
 		s.mNew = s.mOld + (x-s.mOld)/float64(s.count)
 		s.nNew = s.nOld + (x-s.mOld)*(x-s.mNew)
 
 		s.mOld = s.mNew
-		s.nOld = s.mNew
+		s.nOld = s.nNew
 	}
 
 	// @TODO move to internal function
@@ -70,25 +92,44 @@ func (s *WelfordSink) Push(x float64) error {
 			s.observationMin = x
 		}
 	}
+
+	if math.IsInf(s.observationMax, 0) {
+		if x < s.observationMax {
+			s.observationMax = x
+		}
+	} else {
+		if x > s.observationMax {
+			s.observationMax = x
+		}
+	}
+
 	return nil
 }
 
+// Name returns a descriptive comment
 func (s *WelfordSink) Name() string {
 	return welford
 }
 
+// Min returns the smallest observation seen.
 func (s *WelfordSink) Min() float64 {
 	return s.observationMin
 }
 
+// Max returns the largest observation seen.
+func (s *WelfordSink) Max() float64 {
+	return s.observationMax
+}
+
+// Count returns the number of observations that was seen thus far.
 func (s *WelfordSink) Count() int {
 	return s.count
 }
 
+// Mean returns the arithmetic mean of all the observations.
 func (s *WelfordSink) Mean() float64 {
 	if s.count > 0 {
 		return s.mNew
-	} else {
-		return float64(s.count)
 	}
+	return float64(s.count)
 }
--- base_test.go
+++ base_test.go
@@ -1,10 +1,8 @@
 package stats
 
 import (
-	"fmt"
 	"math"
 	"os"
-	"reflect"
 	"testing"
 )
 
@@ -16,23 +14,17 @@ func init() {
 }
 
 func TestMain(m *testing.M) {
-	fmt.Println("testmain")
-	fmt.Println()
+	//fmt.Println("testmain")
 	os.Exit(m.Run())
 }
 
 func TestTypes(t *testing.T) {
-	var s Sink
-	s = NewSink()
-	fmt.Println(reflect.TypeOf(s))
+	var s Sink = NewSink()
 
-	_, ok := s.(Sink)
-	fmt.Printf("%v#\n", ok)
-	if !ok {
+	if _, ok := s.(Sink); !ok {
 		t.Error("not Sink")
 	}
-	_, ok = s.(Variability)
-	if !ok {
+	if _, ok := s.(Variability); !ok {
 		t.Error("not Variability")
 	}
 
@@ -43,48 +35,98 @@ func TestTypes(t *testing.T) {
 }
 
 func isCloseTo(x, target float64) bool {
-	fmt.Printf("%g // %g\n\n", x-target, EPSILON)
+	//fmt.Printf("%g // %g\n\n", x-target, EPSILON)
 	if math.IsInf(x, 0) && math.IsInf(target, 0) {
 		return x == target
-	} else {
-		return math.Abs(x-target) < EPSILON
 	}
+	return math.Abs(x-target) < EPSILON
+}
+
+// round will round up the nearest integer.
+// Source: https://github.com/golang/go/issues/4594#issuecomment-135336012
+// Author: James Hartig https://github.com/fastest963
+func round(f float64) int {
+	if math.Abs(f) < 0.5 {
+		return 0
+	}
+	return int(f + math.Copysign(0.5, f))
+}
+
+// toFixed truncates a float into a specified precision
+// Source: https://stackoverflow.com/a/29786394
+// Author: David Calhoun (Apr 22 15)
+// License: http://creativecommons.org/licenses/by-sa/3.0/
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
 }
 
 func TestZeroValue(t *testing.T) {
-	var v Variability
-	v = NewSink()
-	fmt.Println(reflect.TypeOf(v))
+	var v Variability = NewSink()
 
-	_, ok := v.(Variability)
-	if !ok {
+	if _, ok := v.(Variability); !ok {
 		t.Error("not Variability")
 	}
 	min := v.Min()
 	if !isCloseTo(min, math.Inf(-1)) {
 		t.Errorf("got '%v' ; want '%v'", min, 0.00)
 	}
+	max := v.Max()
+	if !isCloseTo(max, math.Inf(1)) {
+		t.Errorf("got '%v' ; want '%v'", max, 0.00)
+	}
 }
 
 func TestValidValuesAndCounts(t *testing.T) {
 	s := NewSink()
 	if err := s.Push(math.NaN()); err == nil {
-		t.Errorf("expected %v ; got nil", err)
+		t.Errorf("expected %v ; got nil", ErrInvalidValue)
+	}
+	if err := s.Push(math.Inf(1)); err == nil {
+		t.Errorf("expected %v ; got nil", ErrInvalidValue)
 	}
 }
 
-func TestMean(t *testing.T) {
-	s := NewSink()
-	s.Push(5.0)
-	s.Push(3.0)
-	s.Push(1.0)
-	mean := s.Mean()
-	fmt.Printf("mean is %g\n", mean)
-	if !isCloseTo(mean, 3.0) {
-		t.Errorf("got '%v' ; want '%v'", mean, 3.0)
-	}
+type fixtureTest struct {
+	value1       float64
+	value2       float64
+	value3       float64
+	expectedMean float64
+	expectedMin  float64
+	expectedMax  float64
+	expectedSd   float64
+}
 
-	if !isCloseTo(s.Min(), 1.0) {
-		t.Errorf("got '%v' ; want '%v'", s.Min(), 1.0)
+var fixtures = []fixtureTest{
+	{5.0, 3.0, 1.0, 3.0, 1.0, 5.0, 2.0},
+	{2.0, 0.0, 4.0, 2.0, 0.0, 4.0, 2.0},
+	{-1.0, 1.0, 12.0, 4.0, -1.0, 12.0, 7.0},
+	{-1.1813782, -0.2449577, 0.8799429, -0.182, -1.181, 0.880, 1.032},
+	{-0.02735934, -0.62978372, -0.0863240, -0.248, -0.630, -0.027, 0.332},
+}
+
+func TestHappyPath(t *testing.T) {
+	for _, f := range fixtures {
+		s := NewSink()
+		_ = s.Push(f.value1)
+		_ = s.Push(f.value2)
+		_ = s.Push(f.value3)
+		//fmt.Printf("mean %v // min %v // max %v // sd %v\n", s.Mean(), s.Min(), s.Max(), s.StandardDeviation())
+		//fmt.Printf("truncated sd to 3 decimal places %v\n", toFixed(s.StandardDeviation(), 3))
+		if !isCloseTo(toFixed(s.Mean(), 3), f.expectedMean) {
+			t.Errorf("got '%v' ; want '%v'", s.Mean(), f.expectedMean)
+		}
+
+		if !isCloseTo(toFixed(s.Min(), 3), f.expectedMin) {
+			t.Errorf("got '%v' ; want '%v'", s.Min(), f.expectedMin)
+		}
+
+		if !isCloseTo(toFixed(s.Max(), 3), f.expectedMax) {
+			t.Errorf("got '%v' ; want '%v'", s.Max(), f.expectedMax)
+		}
+
+		if !isCloseTo(toFixed(s.StandardDeviation(), 3), f.expectedSd) {
+			t.Errorf("got '%v' ; want '%v'", s.StandardDeviation(), f.expectedSd)
+		}
 	}
 }
